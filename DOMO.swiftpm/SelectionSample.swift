b644d19f3c80a059b5f30903508b90ddf5d8dbcc
ChaeeunShin <shince0805@gmail.com>
2023-03-28 09:30:15 +0900

    [ADD] 선택화면 샘플뷰 생성

---
+++ DOMO.swiftpm/SelectionSample.swift
@@ -0,0 +1,33 @@
+//
+//  SelectionSample.swift
+//  DOMO
+//
+//  Created by Chaeeun Shin on 2023/03/28.
+//
+
+import SwiftUI
+
+enum SelectionSample: CaseIterable {
+    case cute
+    case power
+    case hip
+    case marry
+    
+    var textItem: Text {
+        switch self {
+        case .cute: return Text("『초절정 귀여움』을 보여주지")
+        case .power: return Text("박려크…")
+        case .hip: return Text("나의 힙함을 보여주는 것쯤은 완전 - 여유 - 다.")
+        case .marry: return Text("밀당은 없다 너 하나쯤은 내가 보살펴주마")
+        }
+    }
+    
+    var dstView: some View {
+        switch self {
+        case .cute: return ContentView()
+        case .power: return ContentView()
+        case .hip: return ContentView()
+        case .marry: return ContentView()
+        }
+    }
+}
