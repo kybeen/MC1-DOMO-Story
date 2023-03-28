ac19be5262dbbb119eb615a7bfb51c391ed498f4
psangwon62 <psangwon62@gmail.com>
2023-03-28 17:55:14 +0900

    [REFACTOR] Next Button Refactoring

--- DOMO.swiftpm/SampleViews/ScriptSmapleView.swift
+++ DOMO.swiftpm/SampleViews/ScriptSmapleView.swift
@@ -9,7 +9,7 @@ struct ScriptSampleView: View {
     let screenWidth = UIScreen.main.bounds.size.width
     static let gradientStart = Color(red: 140.0 / 255, green: 89.0 / 255, blue: 181.0 / 255)
     static let gradientEnd = Color(red: 249 / 255, green: 227 / 255, blue: 255 / 255).opacity(0)
-    
+
     // 이름
     let name = "도모쿤♫~♪~!"
     // 대사
@@ -95,14 +95,8 @@ struct ScriptSampleView: View {
                         .padding(.horizontal, screenWidth * 0.05)
                         .animation(.linear(duration: textduration), value: lettersShowing)
                     }
-                    Button {
-                        print("hi")
-                    } label: {
-                        Text(">")
-                            .font(.custom(.DungGeunMo, size: 40))
-                            .foregroundColor(.white)
-                    }
-                    .position(x: 1160, y: 190)
+                    NextButton()
+                        .position(x: 1160, y: 190)
                 }
                 .frame(width: screenWidth, height: screenHeight * 0.3)
             }
