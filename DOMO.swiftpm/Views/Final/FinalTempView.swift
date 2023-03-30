01149c52f7cc2a27e93d5fa6329442d380b6868b
ChaeeunShin <shince0805@gmail.com>
2023-03-30 16:16:08 +0900

    [ADD] 엔딩 테스트 중..

--- DOMO.swiftpm/Views/Final/FinalTempView.swift
+++ DOMO.swiftpm/Views/Final/FinalTempView.swift
@@ -1,24 +1,24 @@
 //
-//  SwiftUIView.swift
+//  FinalTempView.swift
+//  
 //
-//
-//  Created by minseo on 2023/03/29.
+//  Created by Chaeeun Shin on 2023/03/30.
 //
 
-import NavigationStack
 import SwiftUI
+import NavigationStack
 
-struct Final1View: View {
+struct FinalTempView: View {
     @State var lettersShowing: Double = 0
     @State private var textduration: Double = 1.0
     @State var refreshToken: Bool = false
+    
     let screenHeight = UIScreen.main.bounds.size.height
     let screenWidth = UIScreen.main.bounds.size.width
     static let gradientStart = Color(red: 140.0 / 255, green: 89.0 / 255, blue: 181.0 / 255)
     static let gradientEnd = Color(red: 249 / 255, green: 227 / 255, blue: 255 / 255).opacity(0)
 
     let backgroundIamge = "BackgroundFlower"
-    let domoImage = "DomoStand"
     // 이름
     let name = "설레는 도모쿤♫~♪~!"
     // 대사
@@ -31,33 +31,15 @@ struct Final1View: View {
                 .scaledToFill()
                 .frame(height: screenHeight)
 
+            
             VStack {
                 Spacer()
-                HStack {
-                    Spacer()
-                    Image(domoImage)
-                        .rotationEffect(.degrees(30))
-                        // .resizable()
-                        .scaledToFit()
-                        .frame(height: screenHeight * 0.7)
-                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 100, trailing: 100))
-                }
-            }
-
-            VStack {
-                Spacer()
-
                 // 대화창
                 ZStack(alignment: .top) {
                     // 대화창 배경
                     Rectangle()
                         .fill(Color(red: 34 / 255, green: 6 / 255, blue: 56 / 255))
                         .opacity(0.72)
-                        .onTapGesture {
-                            textduration = refreshToken ? 3.0 : 1.0
-                            lettersShowing += Double(script.count)
-                            refreshToken = false
-                        }
                     VStack(alignment: .leading, spacing: 0) {
                         // 대화창 상단
                         HStack {
@@ -78,35 +60,13 @@ struct Final1View: View {
                                 ScriptButtonText(text: "REPLAY")
                                     .padding(.trailing, screenWidth * 0.02)
                             }
-                            PushView(destination: FinalTempView()) {
+                            PushView(destination: EndingView()) {
                                 ScriptButtonText(text: "NEXT")
                                     .padding(.trailing, screenWidth * 0.02)
                             }
                         }
                         .padding(.vertical, screenHeight * 0.03)
-                        Rectangle()
-                            .fill(LinearGradient(
-                                gradient: .init(colors: [Self.gradientStart, Self.gradientEnd]),
-                                startPoint: .init(x: 0, y: 0),
-                                endPoint: .init(x: 1, y: 0)
-                            ))
-                            .frame(width: screenWidth * 0.5, height: screenHeight * 0.015)
-                            .padding(.bottom, screenHeight * 0.03)
-                            .onAppear {
-                                textduration = 1.0
-                                lettersShowing += Double(script.count)
-                            }
-                        // 대사
-                        AppearingText(
-                            fullText: script,
-                            numberOfLettersShow: lettersShowing,
-                            font: .custom(.gulim, size: 35)
-                        )
-                        .lineSpacing(10.0)
-                        .foregroundColor(.white)
-                        .fixedSize()
-                        .padding(.horizontal, screenWidth * 0.05)
-                        .animation(.linear(duration: textduration), value: lettersShowing)
+
                     }
                 }
                 .frame(width: screenWidth, height: screenHeight * 0.3)
@@ -116,9 +76,36 @@ struct Final1View: View {
     }
 }
 
-struct Final1View_Previews: PreviewProvider {
+struct EndingView: View {
+    @State private var isButtonVisible = false
+    static let mintColor = Color(red: 66 / 255, green: 255 / 255, blue: 232 / 255)
+    let logoImage = "Success"
+    var body: some View {
+        ZStack {
+//            FinalTempView()
+            Color.black
+                .opacity(0.6)
+            VStack {
+                Image(logoImage)
+                PushView(destination: MemberView()) {
+                    Text("The end ...")
+                        .font(.custom(.DungGeunMo, size: 70))
+                        .foregroundColor(Self.mintColor)
+                }
+            }
+            .offset(y: isButtonVisible ? 0 : 600)
+            .animation(.easeInOut(duration: 1.5))
+            .onAppear {
+                isButtonVisible = true
+            }
+        }
+    }
+}
+
+struct FinalTempView_Previews: PreviewProvider {
     static var previews: some View {
-        Final1View()
+        FinalTempView()
             .previewInterfaceOrientation(.landscapeLeft)
     }
 }
+
